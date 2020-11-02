c450e577197e17fb20e74693dce6905cd947b624
takeru0219 <takeru0219@gmail.com>
2020-11-02 17:38:48 +0900

    respond words updated

--- reply/src.py
+++ reply/src.py
@@ -2,7 +2,7 @@ import subprocess
 
 from slackbot.bot import listen_to
 
-@listen_to('ドア開けて')
+@listen_to('ドア|非常階段')
 def door_open_request(message):
     # subprocess.call([f'aplay {filename}.wav'], shell = True)
     message.reply('チャイムを鳴らしました。')
