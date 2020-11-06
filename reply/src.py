649dc0082249c62970167d4e745e4be0344d3e35
takeru0219 <takeru0219@gmail.com>
2020-11-06 14:44:01 +0900

    insert logger

--- reply/src.py
+++ reply/src.py
@@ -4,5 +4,6 @@ from slackbot.bot import listen_to
 
 @listen_to('ドア|非常階段')
 def door_open_request(message):
-    subprocess.call([f'aplay', './sound.wav'], shell = True)
+    print('now calling...')
+    subprocess.call([f'aplay', '../sound.wav'], shell = True)
     message.reply('チャイムを鳴らしました。')
